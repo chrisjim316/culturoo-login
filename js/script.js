505fe2ee391cd518ac326092efa8c94458d08a1f
Chris Jim <chrisjim316@gmail.com>
2017-09-30 19:25:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,11 @@
-var api = "https://fcc-weather-api.glitch.me/api/current?";
-
-$(document).ready( function() {
-  
-  /* get the user's consent to use his/her location */
-  /*
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        var lat = "lat=" + position.coords.latitude;
-        var long = "lon=" + position.coords.longitude;
-        var urlString = api + lat + '&' + long;
-        getWeather(urlString);
-        getLocation();
-      });
-    } else {
-      $("#error").append("Unable to retrieve location. Change or update browser version");
-      console.log("Geolocation is not supported for this browser.")
-    }
-  */
+$(document).ready(function () {
+  $("table").hide();
+  $("#login").on("click", function (){
+    $("table").show();
+    return false;
+  });
 });
 
-function getLocation() {
-      $.ajax({
-        url: "https://ipinfo.io/", dataType: "json", success: function (result) {
-          $("#location").append(result.city + " " + result.country); 
-        }  
-      });
-}
-
-function getWeather(urlString) {
-  $.ajax({
-    url: urlString,  dataType: "json", success: function (result) {
-      var temp = result.main.temp.toFixed(1);
-      $("#temp").append(temp);
-      $("#tempUnit").append(String.fromCharCode(176) + "C");
-      $("#description").append(result.weather[0].main + " ");
-      var weatherCondition = result.weather[0].main;
-    }
-  });
-}
-
-  
-
-   
 
 
 
